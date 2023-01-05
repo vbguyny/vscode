6b616a004180e0d3351014da0f30dcb4449fd6c7
VBGuyNY <vb4guy@hotmail.com>
2023-01-05 01:49:45 +0000

    Added Binx project

---
+++ binx/Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("What is your name? ");
+string name = Console.ReadLine()!;
+Console.WriteLine($"Hello, {name}");
