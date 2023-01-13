867077cd156eecc11acc89b075968250c711e2a1
VBGuyNY <vb4guy@hotmail.com>
2023-01-13 01:47:29 +0000

    Wrote full name method

---
+++ fullname/Program.cs
@@ -0,0 +1,39 @@
+﻿
+// static bool IsCoolCat(string name)
+// {
+//     switch (name.ToLower())
+//     {
+//         case "winter":
+//             return true;
+//         default:
+//             return false;
+//     }
+// }
+
+// while (true)
+// {
+//     Console.WriteLine("Enter a cat name: ");
+//     string name = Console.ReadLine()!;
+//     if (IsCoolCat(name))
+//     {
+//         Console.WriteLine($"{name} is a cool cat!");
+//     }
+//     else
+//     {
+//         Console.WriteLine($"I don't know {name}");
+//     }
+// }
+
+while (true)
+{
+    Console.WriteLine("game code...");
+
+    Console.WriteLine("DO you want to play again (Ok)?");
+    string prompt = Console.ReadLine()!.ToLower();
+    if (prompt.ToLower() != "ok")
+    {
+        break;
+    }
+}
+
+Console.Write("Thanks for playing!");
---
+++ methods/Program.cs
@@ -0,0 +1,36 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("What's your name? ");
+        string name = Console.ReadLine();
+
+        bool isCat = IsCat(name);
+        if (isCat)
+        {
+            Console.WriteLine("You are a cat!");
+        }
+        else
+        {
+            Console.WriteLine("You are something else...");
+        }
+    }
+
+    static bool IsCat(string name = "Huh?")
+    {
+        bool isCat = false;
+
+        switch (name.ToLower())
+        {
+            case "thor":
+            case "winter":
+            case "loki":
+            case "luna":
+            case "binx":
+                isCat = true;
+                break;
+        }
+
+        return isCat;
+    }
+}
